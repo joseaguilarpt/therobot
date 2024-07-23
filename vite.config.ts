03a86fa02496d831b20f204ed8c6f690f6f283bb
Jose Aguilar <joseaguilarpt@gmail.com>
2024-07-23 16:46:07 +0100

    improvemnets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import { defineConfig } from 'vite';
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
 });
